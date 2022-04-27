9e98fe6771145d2713dfa374c603b061b6b2c392
mohammadshariful <mohammadsharifulislam64@gmail.com>
2022-04-27 18:14:41 +0600

    added http method

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { use } = require("express/lib/application");
 const app = express();
 const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
-app.use(express());
+app.use(express.json());
 
 //database connected
 
@@ -17,15 +18,48 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const ServicesCollection = client
-    .db("benevolentNetwork")
-    .collection("services");
-  console.log("mongodb connect");
 
-  // perform actions on the collection object
-  client.close();
-});
+async function run() {
+  try {
+    await client.connect();
+    const servicesCollection = client
+      .db("benevolentNetwork")
+      .collection("services");
+    const usersCollection = client.db("benevolentNetwork").collection("users");
+
+    //get operation
+    app.get("/services", async (req, res) => {
+      const query = req.query;
+      const cursor = servicesCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    //post operation
+    app.post("/user", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+    //post user get operation
+    app.get("/user", async (req, res) => {
+      const query = req.query;
+      const cursor = usersCollection.find(query);
+      const users = await cursor.toArray();
+      res.send(users);
+    });
+    // delete user info
+    app.delete("/user/:id", async (req, res) => {
+      const id = req.params;
+      const query = { _id: ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 // init route
 app.get("/", (req, res) => {
